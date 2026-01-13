93f43697899b5b6c22e1595cdae7ddbe8f517e0e
aadhil2002 <aadhilmuhammadabou@gmail.com>
2026-01-13 10:28:55 +0530

    Updates

--- src/services-main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import ServicesApp from "./ServicesApp";
-import "./index.css";
-import "./styles/globals.css";
-
-createRoot(document.getElementById("root")!).render(<ServicesApp />);
